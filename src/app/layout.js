0280832ee6fadb35679fbd9ea36d9a5fbcc72dc8
sajjad1916 <sajjad2.2hasan@gmail.com>
2025-05-23 11:42:31 +0600

    added all the files

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ const inter = Inter({
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
+  weight: ['300', '400', '500', '600', '700'],   
   variable: '--font-poppins',
 });
 
