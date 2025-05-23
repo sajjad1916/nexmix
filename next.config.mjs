dc1c24bf45829867f6f257130dbda341c0839469
sajjad1916 <sajjad2.2hasan@gmail.com>
2025-05-23 11:29:05 +0600

    added all the files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  output: "export",
+  trailingSlash: true, // Ensures proper routing for static sites
+};
 export default nextConfig;
